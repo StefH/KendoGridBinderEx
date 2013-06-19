bf4dd33fffb878bccce8048d7119b907bed56117
Stef Heyenrath <stef.heyenrath@gmail.com>
2013-06-19 23:43:38 +0200

    Update applicationconfig.cs

--- KendoGridBinderEx.Examples.MVC/ApplicationConfig.cs
+++ KendoGridBinderEx.Examples.MVC/ApplicationConfig.cs
@@ -7,29 +7,9 @@ namespace KendoGridBinderEx.Examples.MVC
     {
         private static readonly NameValueCollection Config = ConfigurationManager.AppSettings;
 
-        public static bool RepositoryDeleteAllowed
-        {
-            get { return bool.Parse(Config["Repository.ChangeAllowed"]); }
-        }
-
-        public static bool RepositoryInsertAllowed
-        {
-            get { return bool.Parse(Config["Repository.ChangeAllowed"]); }
-        }
-
-        public static bool RepositoryUpdateAllowed
-        {
-            get { return bool.Parse(Config["Repository.ChangeAllowed"]); }
-        }
-
         public static bool MiniProfilerEnabled
         {
             get { return bool.Parse(Config["MiniProfiler.Enabled"]); }
-        }
-
-        public static bool DatabaseInit
-        {
-            get { return bool.Parse(Config["Database.Init"]); }
         }
     }
 }
