a9f3cddf004c2cab737998b1da186227909dc232
Stef Heyenrath <stef.heyenrath@gmail.com>
2013-11-09 18:07:23 +0100

    EntityFramework 6.0.1

--- KendoGridBinderEx.Examples.Business/Repository/Repository.cs
+++ KendoGridBinderEx.Examples.Business/Repository/Repository.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
 using System.Data.Entity.Infrastructure;
-using System.Data.Objects;
 using System.Linq;
 using System.Linq.Expressions;
 using KendoGridBinderEx.Examples.Business.QueryContext;
--- KendoGridBinderEx.Examples.Business/UnitOfWork/MyDataContext.cs
+++ KendoGridBinderEx.Examples.Business/UnitOfWork/MyDataContext.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
 using System.Data.Entity.Infrastructure;
-using System.Data.Objects;
 using System.Globalization;
 using System.Linq;
 using System.Transactions;
