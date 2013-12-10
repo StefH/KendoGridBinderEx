daa4c41905522d42287a3339dc88d017a052364a
Stef Heyenrath <stef.heyenrath@gmail.com>
2013-12-10 08:40:14 +0100

    Add AccountController file

---
+++ KendoGridBinderEx.Examples.MVC/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using KendoGridBinderEx.Examples.MVC.Models;
+
+namespace KendoGridBinderEx.Examples.MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login(LoginUserVM model)
+        {
+            return View();
+        }
+	}
+}
