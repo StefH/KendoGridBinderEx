1693ab24fe6a5a00551ce319869273c0de0fd64e
StefH <stef.heyenrath@gmail.com>
2014-11-12 21:50:15 +0100

    Fix JQuery UI + background images

--- KendoGridBinderEx.Examples.MVC/App_Start/BundleConfig.cs
+++ KendoGridBinderEx.Examples.MVC/App_Start/BundleConfig.cs
@@ -46,20 +46,7 @@ namespace KendoGridBinderEx.Examples.MVC
 
             // CSS
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                    "~/Content/themes/base/jquery.ui.core.css",
-                    "~/Content/themes/base/jquery.ui.accordion.css",
-                    "~/Content/themes/base/jquery.ui.autocomplete.css",
-                    "~/Content/themes/base/jquery.ui.button.css",
-                    "~/Content/themes/base/jquery.ui.datepicker.css",
-                    "~/Content/themes/base/jquery.ui.dialog.css",
-                    "~/Content/themes/base/jquery.ui.menu.css",
-                    "~/Content/themes/base/jquery.ui.progressbar.css",
-                    "~/Content/themes/base/jquery.ui.resizable.css",
-                    "~/Content/themes/base/jquery.ui.selectable.css",
-                    "~/Content/themes/base/jquery.ui.slider.css",
-                    "~/Content/themes/base/jquery.ui.spinner.css",
-                    "~/Content/themes/base/jquery.ui.tabs.css",
-                    "~/Content/themes/base/jquery.ui.theme.css",
+                    "~/Content/themes/base/all.css",
                     "~/Content/site.css"
                 )
             );
