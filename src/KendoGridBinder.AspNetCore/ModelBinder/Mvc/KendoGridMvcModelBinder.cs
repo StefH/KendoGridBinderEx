1850984047a59a890cb42580c0ad13b7658734b9
Stef Heyenrath <stef.heyenrath@gmail.com>
2019-06-19 10:42:05 +0200

    reformat code

--- src/KendoGridBinder.AspNetCore/ModelBinder/Mvc/KendoGridMvcModelBinder.cs
+++ src/KendoGridBinder.AspNetCore/ModelBinder/Mvc/KendoGridMvcModelBinder.cs
@@ -11,46 +11,46 @@ using System.Threading.Tasks;
 // ReSharper disable once CheckNamespace
 namespace KendoGridBinder.ModelBinder.Mvc
 {
-  public class KendoGridMvcModelBinder : IModelBinder
-  {
-    public Task BindModelAsync([NotNull] ModelBindingContext bindingContext)
+    public class KendoGridMvcModelBinder : IModelBinder
     {
-      if (bindingContext == null)
-      {
-        throw new ArgumentNullException(nameof(bindingContext));
-      }
+        public Task BindModelAsync([NotNull] ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
 
-      bool isForm = bindingContext.HttpContext.Request.HasFormContentType &&
-                    bindingContext.HttpContext.Request.Method.ToUpper() == "POST";
+            bool isForm = bindingContext.HttpContext.Request.HasFormContentType &&
+                          bindingContext.HttpContext.Request.Method.ToUpper() == "POST";
 
-      var collection = isForm ?
-       bindingContext.HttpContext.Request.Form as IEnumerable<KeyValuePair<string, StringValues>> :
-       bindingContext.HttpContext.Request.Query as IEnumerable<KeyValuePair<string, StringValues>>;
+            var collection = isForm ?
+                bindingContext.HttpContext.Request.Form as IEnumerable<KeyValuePair<string, StringValues>> :
+                bindingContext.HttpContext.Request.Query as IEnumerable<KeyValuePair<string, StringValues>>;
 
-      var queryString = new NameValueCollection();
-      foreach (KeyValuePair<string, StringValues> entry in collection)
-      {
-        string key = entry.Key;
-        string value = entry.Value.ToArray().FirstOrDefault();
-        queryString.Add(key, value);
-      }
+            var queryString = new NameValueCollection();
+            foreach (KeyValuePair<string, StringValues> entry in collection)
+            {
+                string key = entry.Key;
+                string value = entry.Value.ToArray().FirstOrDefault();
+                queryString.Add(key, value);
+            }
 
-      var kendoGridRequest = new KendoGridMvcRequest
-      {
-        Take = queryString.GetQueryValue("take", (int?)null),
-        Page = queryString.GetQueryValue("page", (int?)null),
-        Skip = queryString.GetQueryValue("skip", (int?)null),
-        PageSize = queryString.GetQueryValue("pageSize", (int?)null),
+            var kendoGridRequest = new KendoGridMvcRequest
+            {
+                Take = queryString.GetQueryValue("take", (int?)null),
+                Page = queryString.GetQueryValue("page", (int?)null),
+                Skip = queryString.GetQueryValue("skip", (int?)null),
+                PageSize = queryString.GetQueryValue("pageSize", (int?)null),
 
-        FilterObjectWrapper = FilterHelper.Parse(queryString),
-        GroupObjects = GroupHelper.Parse(queryString),
-        AggregateObjects = AggregateHelper.Parse(queryString),
-        SortObjects = SortHelper.Parse(queryString),
-      };
+                FilterObjectWrapper = FilterHelper.Parse(queryString),
+                GroupObjects = GroupHelper.Parse(queryString),
+                AggregateObjects = AggregateHelper.Parse(queryString),
+                SortObjects = SortHelper.Parse(queryString),
+            };
 
-      bindingContext.Result = ModelBindingResult.Success(kendoGridRequest);
+            bindingContext.Result = ModelBindingResult.Success(kendoGridRequest);
 
-      return Task.CompletedTask;
+            return Task.CompletedTask;
+        }
     }
-  }
 }
